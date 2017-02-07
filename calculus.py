23d90158d09fd7dfe6e7c63e0c7f9f5fb0908611
Felix <felix@zollstocks.de>
2017-02-07 02:23:17 +0100

    minor update

---
+++ calculus.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    c = a + b
+    return c
+
+
+def substract(a, b):
+    c = a - b
+    return c
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from calculus import *
 
 
 def halloworld():
@@ -15,3 +16,27 @@ def second():
 
 halloworld()
 second()
+
+print substract(3, 4)
+print sum(4, 3)
+
+class Car(object):
+
+    wheels = 4
+
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
+
+    def set_color(self, color):
+        self.color = color
+
+
+golf = Car('VW', 'Golf', 'blau')
+
+print golf.wheels
+print Car.wheels
+print golf.color
+golf.set_color('rot')
+print golf.color
